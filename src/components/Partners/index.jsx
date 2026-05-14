bc3d5f70d3f01b521584209f0267f1e698734990
Roman <44591647+koffcheg@users.noreply.github.com>
2026-05-14 12:53:27 +0300

    Structure partners marquee as equal logo groups

--- src/components/Partners/index.jsx
+++ src/components/Partners/index.jsx
@@ -1,8 +1,6 @@
 import { PARTNERS } from '../../data';
 
 export function Partners() {
-  const logos = [...PARTNERS, ...PARTNERS];
-
   return (
     <section
       className="partners"
@@ -10,18 +8,25 @@ export function Partners() {
     >
       <div className="partners-slider">
         <div className="partners-slider-track">
-          {logos.map(({ id, image }, index) => (
+          {[0, 1].map(groupIndex => (
             <div
-              key={`${id}-${index}`}
-              className="partners-slider-item"
-              aria-hidden={index >= PARTNERS.length ? 'true' : undefined}
+              key={groupIndex}
+              className="partners-slider-group"
+              aria-hidden={groupIndex > 0 ? 'true' : undefined}
             >
-              <img
-                src={image}
-                alt=""
-                loading="lazy"
-                draggable="false"
-              />
+              {PARTNERS.map(({ id, image }) => (
+                <div
+                  key={`${id}-${groupIndex}`}
+                  className="partners-slider-item"
+                >
+                  <img
+                    src={image}
+                    alt=""
+                    loading="lazy"
+                    draggable="false"
+                  />
+                </div>
+              ))}
             </div>
           ))}
         </div>
